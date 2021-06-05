41915e1cbaf286644862f42924ba83ca15ac32f3
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-05 20:35:09 +0200

    Rework BI Q1

--- postgres/queries/bi-1.sql
+++ postgres/queries/bi-1.sql
@@ -1,33 +1,30 @@
 /* Q1. Posting summary
 \set datetime '\'2011-07-21T22:00:00.000+00:00\''::timestamp
  */
-WITH 
+WITH
   message_count AS (
     SELECT 0.0 + count(*) AS cnt
       FROM message
-     WHERE 1=1
-       AND m_creationdate < :datetime
+     WHERE creationDate < :datetime
 )
 , message_prep AS (
-    SELECT extract(year from m_creationdate) AS messageYear
-         , m_c_replyof IS NOT NULL AS isComment
+    SELECT extract(year from creationDate) AS messageYear
+         , ParentMessageId IS NOT NULL AS isComment
          , CASE
-             WHEN m_length <  40 THEN 0 -- short
-             WHEN m_length <  80 THEN 1 -- one liner
-             WHEN m_length < 160 THEN 2 -- tweet
+             WHEN length <  40 THEN 0 -- short
+             WHEN length <  80 THEN 1 -- one liner
+             WHEN length < 160 THEN 2 -- tweet
              ELSE                     3 -- long
            END AS lengthCategory
-         , m_length
+         , length
       FROM message
-     WHERE 1=1
-       AND m_creationdate < :datetime
-       --AND m_content IS NOT NULL
-       AND m_ps_imagefile IS NULL -- FIXME CHECKME: posts w/ m_ps_imagefile IS NOT NULL should have m_content IS NULL
+     WHERE creationDate < :datetime
+       AND content IS NOT NULL -- TODO NULL vs empty string
 )
 SELECT messageYear, isComment, lengthCategory
      , count(*) AS messageCount
-     , avg(m_length) AS averageMessageLength
-     , sum(m_length) AS sumMessageLength
+     , avg(length) AS averageMessageLength
+     , sum(length) AS sumMessageLength
      , count(*) / mc.cnt AS percentageOfMessages
   FROM message_prep
      , message_count mc
