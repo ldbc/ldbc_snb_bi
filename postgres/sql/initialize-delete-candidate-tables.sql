d114f17762e706b2f632fa00fa5eb2fdc183f4fd
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-10 03:18:05 +0200

    Remove deprecated scripts

--- postgres/sql/initialize-delete-candidate-tables.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Person_Delete_candidates                (deletionDate timestamp without time zone not null, id bigint);
-CREATE TABLE IF NOT EXISTS Forum_Delete_candidates                 (deletionDate timestamp without time zone not null, id bigint);
-CREATE TABLE IF NOT EXISTS Comment_Delete_candidates               (deletionDate timestamp without time zone not null, id bigint);
-CREATE TABLE IF NOT EXISTS Post_Delete_candidates                  (deletionDate timestamp without time zone not null, id bigint);
-CREATE TABLE IF NOT EXISTS Person_likes_Comment_Delete_candidates  (deletionDate timestamp without time zone not null, src bigint, trg bigint);
-CREATE TABLE IF NOT EXISTS Person_likes_Post_Delete_candidates     (deletionDate timestamp without time zone not null, src bigint, trg bigint);
-CREATE TABLE IF NOT EXISTS Forum_hasMember_Person_Delete_candidates(deletionDate timestamp without time zone not null, src bigint, trg bigint);
-CREATE TABLE IF NOT EXISTS Person_knows_Person_Delete_candidates   (deletionDate timestamp without time zone not null, src bigint, trg bigint);
