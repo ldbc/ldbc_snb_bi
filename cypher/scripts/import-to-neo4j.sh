cd7cb9c34ac450bd6c3779c7c9ea6d37d527fa34
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-11 11:10:12 +0200

    Set error tolerance to zero in Cypher loader

--- cypher/scripts/import-to-neo4j.sh
+++ cypher/scripts/import-to-neo4j.sh
@@ -55,6 +55,7 @@ docker run --rm \
     neo4j-admin import \
     --id-type=INTEGER \
     --ignore-empty-strings=true \
+    --bad-tolerance=0 \
     --nodes=Place="/headers/static/Place.csv$(find ${NEO4J_CSV_DIR}/initial_snapshot/static/Place -type f -name 'part-*.csv' -printf ',/import/initial_snapshot/static/Place/%f')" \
     --nodes=Organisation="/headers/static/Organisation.csv$(find ${NEO4J_CSV_DIR}/initial_snapshot/static/Organisation -type f -name 'part-*.csv' -printf ',/import/initial_snapshot/static/Organisation/%f')" \
     --nodes=TagClass="/headers/static/TagClass.csv$(find ${NEO4J_CSV_DIR}/initial_snapshot/static/TagClass -type f -name 'part-*.csv' -printf ',/import/initial_snapshot/static/TagClass/%f')" \
