32a1261f66375c923401da07f7aa0835408b624d
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-25 09:28:25 +0100

    Remove unused directory

--- cypher/driver/bi-benchmark.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -cp target/cypher-0.4.0-SNAPSHOT.jar com.ldbc.driver.Client -dm EXECUTE_WORKLOAD -P bi-benchmark.properties
--- cypher/driver/bi-create-validation-parameters.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -cp target/cypher-0.4.0-SNAPSHOT.jar com.ldbc.driver.Client -dm CREATE_VALIDATION_PARAMS -P bi-create-validation-parameters.properties
