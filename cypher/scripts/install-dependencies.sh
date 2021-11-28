fea59139d5126d6b1f17cc2d33ca1819f6981181
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-28 22:03:34 +0100

    Sync version in Cypher dependency installation script with the default version used

--- cypher/scripts/install-dependencies.sh
+++ cypher/scripts/install-dependencies.sh
@@ -3,4 +3,7 @@
 set -e
 set -o pipefail
 
-pip3 install -U neo4j==4.2.1 python-dateutil
+cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
+. environment-variables-default.sh
+
+pip3 install -U neo4j==${NEO4J_VERSION} python-dateutil
