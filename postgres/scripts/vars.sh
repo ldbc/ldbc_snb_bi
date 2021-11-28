ab355b7a96735768c62c1ae9a6d43a0dea59ddd0
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-28 22:16:02 +0100

    Bump Postgres version

--- postgres/scripts/vars.sh
+++ postgres/scripts/vars.sh
@@ -3,7 +3,7 @@ pushd .
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-export POSTGRES_VERSION=13.3
+export POSTGRES_VERSION=14.1
 export POSTGRES_CONTAINER_NAME=snb-bi-postgres
 export POSTGRES_PASSWORD=mysecretpassword
 export POSTGRES_DATABASE=ldbcsnb
