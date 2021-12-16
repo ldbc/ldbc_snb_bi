d0536071e06af043339feed74d49168234b5e17a
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-16 09:28:01 +0100

    Add missing Python dependencies

--- cypher/scripts/install-dependencies.sh
+++ cypher/scripts/install-dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip3 install -U neo4j
+pip3 install --user neo4j==${NEO4J_VERSION} python-dateutil
--- postgres/scripts/install-dependencies.sh
+++ postgres/scripts/install-dependencies.sh
@@ -5,4 +5,4 @@ set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-pip3 install --user --progress-bar off psycopg2-binary
+pip3 install --user --progress-bar off psycopg2-binary python-dateutil
