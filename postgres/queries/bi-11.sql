e25307f26a66d966b282e66401a0d4287fd179bb
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-19 14:08:46 +0100

    Update Q17 implementations to use a date range instead of a single date constraint

--- postgres/queries/bi-11.sql
+++ postgres/queries/bi-11.sql
@@ -1,6 +1,7 @@
 /* Q11. Friend triangles
 \set country '\'Belarus\''
 \set startDate '\'2010-06-01\''::timestamp
+\set endDate '\'2010-07-01\''::timestamp
  */
 WITH Persons_of_country_w_friends AS (
     SELECT Person.id AS PersonId
@@ -27,7 +28,7 @@ SELECT count(*)
    AND p1.PersonId < p2.PersonId
    AND p2.PersonId < p3.PersonId
     -- filter: only edges created after :startDate
-   AND :startDate <= p1.creationDate
-   AND :startDate <= p2.creationDate
-   AND :startDate <= p3.creationDate
+   AND :startDate <= p1.creationDate AND p1.creationDate <= :endDate
+   AND :startDate <= p2.creationDate AND p2.creationDate <= :endDate
+   AND :startDate <= p3.creationDate AND p3.creationDate <= :endDate
 ;
