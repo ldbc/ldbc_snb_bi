d5ea7eee018f2f273d793afbf1aa0f31a4805aaf
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-19 13:43:13 +0100

    Allow overriding Neo4j's version and environment variables with external env vars

--- cypher/scripts/vars.sh
+++ cypher/scripts/vars.sh
@@ -4,9 +4,9 @@ cd ..
 export NEO4J_CONTAINER_NAME=snb-bi-neo4j
 export NEO4J_CONTAINER_ROOT=`pwd`/scratch
 export NEO4J_DATA_DIR=${NEO4J_CONTAINER_ROOT}/data
-export NEO4J_ENV_VARS=""
+export NEO4J_ENV_VARS=${NEO4J_ENV_VARS:-}
 export NEO4J_HEADER_DIR=`pwd`/headers
-export NEO4J_VERSION=4.4.0
+export NEO4J_VERSION=${NEO4J_VERSION:-4.4.1}
 
 if [ ! -v NEO4J_CSV_FLAGS ]; then
     export NEO4J_CSV_FLAGS=""
