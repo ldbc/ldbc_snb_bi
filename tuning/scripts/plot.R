6208185bbd77bfbeb1f2a79cc6ac191e2bac2c16
jackwaudby <j.waudby2@ncl.ac.uk>
2022-02-09 15:20:22 +0100

    use require in r scripts

--- tuning/scripts/plot.R
+++ tuning/scripts/plot.R
@@ -1,27 +1,26 @@
-library(ggplot2)
-library(readr)
-library(patchwork)
+require(ggplot2)
+require(readr)
+require(patchwork)
 
 # load data
 queries = c("1", "2a", "2b", "3", "4", "5", "6", "7", "8a", "8b", "9", "10a", "10b", "11", "12", "13", "14a", "14b", "15a", "15b", "16a", "16b", "17",
             "18", "19a", "19b", "20")
 for (q in queries) {
   file = paste0("./data/bi-", q,"-summary.csv")
-  
+
   df = read_csv(file = file, col_names = T)
 
   # histogram
-  p1 = ggplot(df, aes(x=dbHits)) + 
-    geom_histogram(color="black", fill="white") + 
+  p1 = ggplot(df, aes(x=dbHits)) +
+    geom_histogram(color="black", fill="white") +
     theme_bw()
-  
-  p2 = ggplot(df, aes(x=records)) + 
-    geom_histogram(color="black", fill="white") + 
+
+  p2 = ggplot(df, aes(x=records)) +
+    geom_histogram(color="black", fill="white") +
     theme_bw()
-  
+
   combined <- p1 + p2  & theme(text = element_text(size = 20))
   combined + plot_layout(guides = "collect")
-  
+
   ggsave(paste0("./graphics/bi-", q, "-summary.pdf"),width = 12, height = 6,device = "pdf")
 }
-
