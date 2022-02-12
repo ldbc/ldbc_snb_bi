3860d1e83a5e8fbbcecf3c566d3aec87069642ca
jackwaudby <j.waudby2@ncl.ac.uk>
2022-02-12 11:53:57 +0100

    param query 9

--- paramgen/parameter-queries/pq-9.sql
+++ paramgen/parameter-queries/pq-9.sql
@@ -1,5 +1,6 @@
 SELECT
-    creationDayNumMessages.creationDay AS 'startDate:DATE',
-    creationDayNumMessages.creationDay + 8 + CAST(FLOOR(3*RANDOM()) AS INT) AS 'endDate:DATE'
-FROM
-    (SELECT * FROM creationDayNumMessages LIMIT 400) creationDayNumMessages
+      anchorDate::date + INTERVAL (- 5 + CAST(FLOOR(10*RANDOM()) AS INT)) DAY  AS 'startDate:DATE',
+      anchorDate::date + INTERVAL (80+CAST(FLOOR(20*RANDOM()) AS INT)) DAY AS 'endDate:DATE'
+FROM (
+     SELECT percentile_disc(0.89) WITHIN GROUP (ORDER BY creationDay) AS anchorDate FROM creationDayNumMessages ),
+    (SELECT unnest(generate_series(1, 10)))
