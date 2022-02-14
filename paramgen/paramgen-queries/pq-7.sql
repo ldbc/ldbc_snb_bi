1bb14765ed2f0e8c126820de5edf3d6c6cea9c9b
jackwaudby <j.waudby2@ncl.ac.uk>
2022-02-14 16:31:51 +0100

    change pq7 percentile

--- paramgen/paramgen-queries/pq-7.sql
+++ paramgen/paramgen-queries/pq-7.sql
@@ -3,9 +3,9 @@ FROM (
     SELECT
         tagName,
         frequency AS freq,
-        abs(frequency - (SELECT percentile_disc(0.92) WITHIN GROUP (ORDER BY frequency) FROM tagNumMessages)) AS diff
+        abs(frequency - (SELECT percentile_disc(0.98) WITHIN GROUP (ORDER BY frequency) FROM tagNumMessages)) AS diff
     FROM tagNumMessages
     ORDER BY diff, tagName
-    LIMIT 400
+    LIMIT 200
 )
 ORDER BY md5(tagName)
