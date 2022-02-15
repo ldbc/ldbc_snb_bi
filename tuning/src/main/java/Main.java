14fae723881820d95e2f11c4523ee21819e55a62
jackwaudby <j.waudby2@ncl.ac.uk>
2022-02-16 00:16:53 +0100

    add 15 to non parallel runtime

--- tuning/src/main/java/Main.java
+++ tuning/src/main/java/Main.java
@@ -107,7 +107,7 @@ public class Main implements AutoCloseable
         var actual = Files.readString( fileName );
         var split = Arrays.asList( actual.split( "\\*/", 2 ) );
 
-        var nonParallelRuntime = new HashSet<>( Arrays.asList( "2", "4", "10", "13", "16", "17", "18", "19", "20" ) );
+        var nonParallelRuntime = new HashSet<>( Arrays.asList( "2", "4", "10", "13", "15a", "15b", "16", "17", "18", "19", "20" ) );
 
         String result;
         if ( nonParallelRuntime.contains( queryNum ) )
