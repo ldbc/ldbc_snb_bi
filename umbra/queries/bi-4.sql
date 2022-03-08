f9896a55db68513ae755be2dc95f78b04a9701ea
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-08 15:47:43 +0100

    Q4 opt

--- umbra/queries/bi-4.sql
+++ umbra/queries/bi-4.sql
@@ -13,7 +13,7 @@ WITH Top100_Popular_Forums AS (
       JOIN Country
         ON Country.id = City.PartOfCountryId
       JOIN Forum
-        ON Forum_hasMember_Person.ForumId = Forum.id  
+        ON Forum_hasMember_Person.ForumId = Forum.id
        AND Forum.creationDate > :date
       GROUP BY Country.Id, Forum.Id
   ) ForumMembershipPerCountry
@@ -26,17 +26,17 @@ SELECT au.id AS "person.id"
      , au.lastName AS "person.lastName"
      , au.creationDate
      -- a single person might be member of more than 1 of the top100 forums, so their messages should be DISTINCT counted
-     , count(DISTINCT MessageThread.MessageId) AS messageCount
-  FROM Top100_Popular_Forums
-       INNER JOIN Forum_hasMember_Person
-               ON Top100_Popular_Forums.id = Forum_hasMember_Person.ForumId
-       -- author of the message
-       INNER JOIN Person au
-               ON Forum_hasMember_Person.PersonId = au.id
+     , count(MessageThread.MessageId) AS messageCount
+  FROM
+       Person au
        LEFT JOIN MessageThread
               ON au.id = MessageThread.CreatorPersonId
              AND MessageThread.ContainerForumId IN (SELECT id FROM Top100_Popular_Forums)
              AND MessageThread.creationDate > :date
+  WHERE EXISTS (SELECT * FROM Top100_Popular_Forums INNER JOIN
+                Forum_hasMember_Person ON Top100_Popular_Forums.id =
+                Forum_hasMember_Person.ForumId WHERE Forum_hasMember_Person.PersonId = au.id
+)
 GROUP BY au.id, au.firstName, au.lastName, au.creationDate
 ORDER BY messageCount DESC, au.id
 LIMIT 100
