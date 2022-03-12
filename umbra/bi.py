3db541c7b26b49e54502602ca3a08a374c8fc52c
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-12 14:11:45 +0100

    Print Umbra results

--- umbra/bi.py
+++ umbra/bi.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import csv
+import datetime
 import os
 import re
 import psycopg2
@@ -8,9 +8,50 @@ import sys
 
 # Usage: bi.py [--test]
 
-def run_query(con, query_id, query_spec, query_parameters):
-    if test:
-        print(f'Q{query_id}: {query_parameters}')
+result_mapping = {
+     1: ["INT32", "BOOL", "INT32", "INT32", "FLOAT32", "INT32", "FLOAT32"],
+     2: ["STRING", "INT32", "INT32", "INT32"],
+     3: ["ID", "STRING", "DATETIME", "ID", "INT32"],
+     4: ["ID", "STRING", "STRING", "DATETIME", "INT32"],
+     5: ["ID", "INT32", "INT32", "INT32", "INT32"],
+     6: ["ID", "INT32"],
+     7: ["STRING", "INT32"],
+     8: ["ID", "INT32", "INT32"],
+     9: ["ID", "STRING", "STRING", "INT32", "INT32"],
+    10: ["ID", "STRING", "INT32"],
+    11: ["INT64"],
+    12: ["INT32", "INT32"],
+    13: ["ID", "INT32", "INT32", "FLOAT32"],
+    14: ["ID", "ID", "STRING", "INT32"],
+    15: ["ID[]", "FLOAT32"],
+    16: ["ID", "INT32", "INT32"],
+    17: ["ID", "INT32"],
+    18: ["ID", "INT32"],
+    19: ["ID", "ID", "FLOAT32"],
+    20: ["ID", "INT64"],
+}
+
+def convert_value_to_string(value, result_type, input):
+    if result_type == "ID[]" or result_type == "INT[]" or result_type == "INT32[]" or result_type == "INT64[]":
+        return value.replace("{", "[").replace("}", "]").replace(";", ",")
+    elif result_type == "ID" or result_type == "INT" or result_type == "INT32" or result_type == "INT64":
+        return str(int(value))
+    elif result_type == "FLOAT" or result_type == "FLOAT32" or result_type == "FLOAT64":
+        return str(float(value))
+    elif result_type == "STRING[]":
+        return "[" + ";".join([f'"{v}"' for v in value]) + "]"
+    elif result_type == "STRING":
+        return f'"{value}"'
+    elif result_type == "DATETIME":
+        return f"{datetime.datetime.strftime(value, '%Y-%m-%dT%H:%M:%S.%f')[:-3]}+00:00"
+    elif result_type == "DATE":
+        return datetime.datetime.strftime(value, '%Y-%m-%d')
+    elif result_type == "BOOL":
+        return str(bool(value))
+    else:
+        raise ValueError(f"Result type {result_type} not found")
+
+def run_query(con, query_num, query_spec, query_parameters):
     start = time.time()
     cur = con.cursor()
 
@@ -21,17 +62,25 @@ def run_query(con, query_id, query_spec, query_parameters):
     results = cur.fetchall()
     end = time.time()
     duration = end - start
+
+    mapping = result_mapping[query_num]
+    result_tuples = "[" + ";".join([
+            f'<{",".join([convert_value_to_string(result[i], type, False) for i, type in enumerate(mapping)])}>'
+            for result in results
+        ]) + "]"
+    print("-> " + result_tuples)
+
     if test:
         print(f"-> {duration:.4f} seconds")
         print(f"-> {results}")
     return (results, duration)
 
 def convert_to_datetime(timestamp):
-    dt = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f+00:00")
+    dt = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f+00:00")
     return f"'{dt}'::timestamp"
 
 def convert_to_date(timestamp):
-    dt = datetime.strptime(timestamp, '%Y-%m-%d')
+    dt = datetime.datetime.strptime(timestamp, '%Y-%m-%d')
     return f"'{dt}'::date"
 
 sf = os.environ.get("SF")
@@ -72,7 +121,7 @@ for query_variant in ["1", "2a", "2b", "3", "4", "5", "6", "7", "8a", "8b", "9",
         # drop type designators
         type_pattern = re.compile(':.*')
         query_parameters = {type_pattern.sub('', k): v for k, v in query_parameters.items()}
-        (results, duration) = run_query(con, query_variant, query_spec, query_parameters)
+        (results, duration) = run_query(con, query_num, query_spec, query_parameters)
 
         timings_file.write(f"{sf}|{query_variant}|{duration}\n")
         timings_file.flush()
