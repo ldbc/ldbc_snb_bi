ebe62b894adb540197a2da3c9a22e9ce8e4dde7c
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-14 21:12:44 +0100

    Adjust parameters

--- paramgen/paramgen-queries/pq-17.sql
+++ paramgen/paramgen-queries/pq-17.sql
@@ -5,7 +5,7 @@ FROM (
     SELECT
         tagName,
         frequency AS freq,
-        abs(frequency - (SELECT percentile_disc(0.52) WITHIN GROUP (ORDER BY frequency) FROM tagNumMessages)) AS diff
+        abs(frequency - (SELECT percentile_disc(0.27) WITHIN GROUP (ORDER BY frequency) FROM tagNumMessages)) AS diff
     FROM tagNumMessages
     ORDER BY diff, tagName
     LIMIT 400
