bc1340ec7af442b53e842ea4bfe28869aec9df27
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-19 22:06:04 +0100

    Umbra: Fix Q18 implementation - person2 was not connected to tag

--- umbra/queries/bi-18.sql
+++ umbra/queries/bi-18.sql
@@ -5,15 +5,21 @@ SELECT k1.Person1Id AS "person1.id", k2.Person2Id AS "person2.id", count(DISTINC
 FROM Person_knows_Person k1
 JOIN Person_knows_Person k2
   ON k1.Person2Id = k2.Person1Id -- pattern: mutualFriend
-JOIN Person_hasInterest_Tag
-  ON Person_hasInterest_Tag.PersonId = k2.Person2Id
+JOIN Person_hasInterest_Tag pt1
+  ON pt1.PersonId = k2.Person2Id -- pattern: person2
+JOIN Person_hasInterest_Tag pt2
+  ON pt2.PersonId = k1.Person1Id -- pattern: person1
 JOIN Tag
-  ON Person_hasInterest_Tag.TagId = Tag.id
- AND Tag.name = :tag
-LEFT JOIN Person_knows_Person k3
-       ON k3.Person1Id = k2.Person2Id -- pattern: person2
-      AND k3.Person2Id = k1.Person1Id -- pattern: person1
+  ON Tag.name = :tag
+ AND pt1.TagId = Tag.id
+ AND pt2.TagId = Tag.id
+ -- negative edge
 WHERE k1.Person1Id != k2.Person2Id
+  AND NOT EXISTS (SELECT 1
+         FROM Person_knows_Person k3
+        WHERE k3.Person1Id = k2.Person2Id -- pattern: person2
+          AND k3.Person2Id = k1.Person1Id -- pattern: person1
+      )
 GROUP BY k1.Person1Id, k2.Person2Id
 ORDER BY mutualFriendCount DESC, k1.Person1Id ASC, k2.Person2Id ASC
 LIMIT 20
