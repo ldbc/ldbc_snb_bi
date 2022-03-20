85aac451f41efc00ae9367fe3a2dc020b53ae072
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-20 19:25:45 +0100

    Add formatting to Q1 params

--- naive-paramgen/paramgen-queries/pq-1.sql
+++ naive-paramgen/paramgen-queries/pq-1.sql
@@ -1,4 +1,4 @@
-SELECT creationDay AS 'datetime:DATETIME'
+SELECT strftime(creationDay, '%Y-%m-%dT%H:%M:%S.%g+00:00') AS 'datetime:DATETIME'
 FROM creationDayNumMessages
 ORDER BY md5(creationDay)
 LIMIT 400
