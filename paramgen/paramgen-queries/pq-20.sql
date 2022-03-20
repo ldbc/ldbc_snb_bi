96da5bef796e4979596390cd8ad0ccd9f4a2e71c
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-20 16:11:22 +0100

    Format query

--- paramgen/paramgen-queries/pq-20.sql
+++ paramgen/paramgen-queries/pq-20.sql
@@ -11,11 +11,10 @@ FROM
     ORDER BY diff, companyId
     LIMIT 50) comp,
     (SELECT DISTINCT person2Id FROM PersonDisjointEmployerPairs) pers2
-WHERE NOT EXISTS (
-        SELECT 1
+WHERE NOT EXISTS (SELECT 1
         FROM PersonDisjointEmployerPairs aj
         WHERE aj.companyId = comp.companyId
           AND aj.person2Id = pers2.person2Id
-    )
+        )
 ORDER BY md5(3532569367*comp.companyId + 211*pers2.person2Id)
 LIMIT 400
