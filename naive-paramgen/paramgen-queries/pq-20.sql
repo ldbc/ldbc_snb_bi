b983900ea2a85ba9ee6ca36cf952bcde7f09895d
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-20 19:23:50 +0100

    Limit persons in naive paramgen PQ20

--- naive-paramgen/paramgen-queries/pq-20.sql
+++ naive-paramgen/paramgen-queries/pq-20.sql
@@ -3,6 +3,6 @@ SELECT
     personId AS 'person2Id:ID'
 FROM
     companyNumEmployees,
-    personNumFriends
+    (SELECT personId FROM personNumFriends ORDER BY personId LIMIT 100)
 ORDER BY md5(3532569367*companyId + 211*personId)
 LIMIT 400
