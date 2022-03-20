9488ef20d63bda9c11046aade9c4ef7e296ee131
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-20 22:48:30 +0100

    Add missing fields to naive paramgen Q15a/b

--- naive-paramgen/paramgen-queries/pg-15a.sql
+++ naive-paramgen/paramgen-queries/pg-15a.sql
@@ -1,6 +1,8 @@
 SELECT
     p1.personId AS 'person1Id:ID',
-    p2.personId AS 'person2Id:ID'
+    p2.personId AS 'person2Id:ID',
+    (SELECT creationDay FROM creationDayNumMessages ORDER BY md5(creationDay) ASC ) - INTERVAL (p1.personId % 7) DAY AS 'startDate:DATE',
+    (SELECT creationDay FROM creationDayNumMessages ORDER BY md5(creationDay) DESC) + INTERVAL (p2.personId % 7) DAY AS 'endDate:DATE'
 FROM
     (SELECT personId FROM personNumFriends ORDER BY md5(personId) ASC  LIMIT 20) p1,
     (SELECT personId FROM personNumFriends ORDER BY md5(personId) DESC LIMIT 20) p2
--- naive-paramgen/paramgen-queries/pg-15b.sql
+++ naive-paramgen/paramgen-queries/pg-15b.sql
@@ -1,6 +1,8 @@
 SELECT
     p1.personId AS 'person1Id:ID',
-    p2.personId AS 'person2Id:ID'
+    p2.personId AS 'person2Id:ID',
+    (SELECT creationDay FROM creationDayNumMessages ORDER BY md5(creationDay) ASC ) - INTERVAL (-11 + p1.personId % 9) DAY AS 'startDate:DATE',
+    (SELECT creationDay FROM creationDayNumMessages ORDER BY md5(creationDay) DESC) + INTERVAL ( 11 + p2.personId % 9) DAY AS 'endDate:DATE'
 FROM
     (SELECT personId FROM personNumFriends ORDER BY md5(personId) ASC  LIMIT 20) p1,
     (SELECT personId FROM personNumFriends ORDER BY md5(personId) DESC LIMIT 20) p2
