7d5a816f01dba137d0d78066968e627a91ca3105
Gabor Szarnyas <szarnyasg@gmail.com>
2022-04-12 12:20:23 +0200

    Load Umbra container in CI

--- umbra/scripts/vars.sh
+++ umbra/scripts/vars.sh
@@ -8,7 +8,8 @@ export UMBRA_DATABASE_DIR=`pwd`/scratch/db/
 export UMBRA_LOG_DIR=`pwd`/scratch/log/
 export UMBRA_DDL_DIR=`pwd`/ddl/
 export UMBRA_CONTAINER_NAME=snb-bi-umbra
-export UMBRA_DOCKER_IMAGE=umbra-release:533b93fd2
+export UMBRA_VERSION=533b93fd2
+export UMBRA_DOCKER_IMAGE=umbra-release:${UMBRA_VERSION}
 
 cd scripts
 
