b0ccc8dd528cb94dd1fd7a19186f5311be881a40
Gabor Szarnyas <szarnyasg@gmail.com>
2022-04-25 22:11:53 +0200

    Optimize Q20m query

--- paramgen/paramgen-queries/pg-20m.sql
+++ paramgen/paramgen-queries/pg-20m.sql
@@ -1,9 +1,12 @@
 -- materialize table
 CREATE TABLE same_university_knows AS
-    SELECT k.person1id AS person1Id, k.person2id AS person2Id
-    FROM knows_window k
-    JOIN Person_studyAt_Univesity_window p1
-      ON p1.personId = k.person1Id
+    SELECT p1joined.person1id AS person1Id, p1joined.person2id AS person2Id
+    FROM (
+      SELECT k.person1Id AS person1Id, k.person2Id AS person2Id, p1.universityId AS universityId
+      FROM knows_window k
+      JOIN Person_studyAt_Univesity_window p1
+        ON p1.personId = k.person1Id
+    ) p1joined
     JOIN Person_studyAt_Univesity_window p2
-      ON p2.personId = k.person2Id
-     AND p2.universityId = p1.universityId
+      ON p2.personId = p1joined.person2Id
+     AND p2.universityId = p1joined.universityId
