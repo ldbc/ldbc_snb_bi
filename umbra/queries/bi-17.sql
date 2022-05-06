0559f6e535886b149201406c8b83444d3158dd79
Altan Birler <altan.birler@tum.de>
2022-05-06 23:12:53 +0200

    Optimize Umbra queries
    The shortest path queries Q15, Q19, Q20 now do proper two way search.
    
    Unnecessary joins were removed from Q17.

--- umbra/queries/bi-17.sql
+++ umbra/queries/bi-17.sql
@@ -2,50 +2,33 @@
 \set tag '\'Slavoj_Žižek\''
 \set delta '4'
  */
+WITH MyMessage as (
+    SELECT *
+    FROM Message
+-- (tag)<-[:HAS_TAG]-(message)
+    WHERE MessageId in (SELECT MessageId FROM Message_hasTag_Tag WHERE TagId IN (SELECT id FROM Tag WHERE Tag.name = :tag))
+)
 -- (message1)-[:HAS_CREATOR]->(person1)
 SELECT Message1.CreatorPersonId AS "person1.id", count(Message2.MessageId) AS messageCount
-FROM Tag
--- (tag)<-[:HAS_TAG]-(message1)
-JOIN Message_hasTag_Tag Message1_hasTag_Tag
-  ON Message1_hasTag_Tag.TagId = Tag.id
-JOIN Message Message1
-  ON Message1.MessageId = Message1_hasTag_Tag.MessageId
--- (message1)-[:REPLY_OF*0..]->(post1)
-JOIN Post_View Post1
-  ON Post1.id = Message1.RootPostId
--- (tag)<-[:HAS_TAG]-(message2)
-JOIN Message_hasTag_Tag Message2_hasTag_Tag
-  ON Message2_hasTag_Tag.TagId = Tag.id
+FROM MyMessage Message1
 -- (message2 <date filtering>})
-JOIN Message Message2
-  ON Message2.MessageId = Message2_hasTag_Tag.MessageId
- AND (Message1.creationDate + ':delta hour'::interval) < Message2.creationDate
--- (tag)<-[:HAS_TAG]-(comment)
-JOIN Message_hasTag_Tag Comment_hasTag_Tag
-  ON Comment_hasTag_Tag.TagId = Tag.id
--- (comment)-[:REPLY_OF]->(message2)
-JOIN Comment_View Comment
-  ON Comment.id = Comment_hasTag_Tag.MessageId
- AND coalesce(Comment.ParentPostId, Comment.ParentCommentId) = Message2.MessageId
--- (message2)-[:REPLY_OF*0..]-(post2)
-JOIN Post_View Post2
-  ON Post2.id = Message2.RootPostId
- AND Post2.ContainerForumId != Post1.ContainerForumId -- forum2 != forum1
+JOIN MyMessage Message2
+ ON (Message1.creationDate + ':delta hour'::interval) < Message2.creationDate
+JOIN MyMessage Comment
+ ON coalesce(Comment.ParentPostId, Comment.ParentCommentId) = Message2.MessageId
 -- (forum1)-[:Has_MEMBER]->(person2)
 JOIN Forum_hasMember_Person Forum_hasMember_Person2
-  ON Forum_hasMember_Person2.ForumId = Post1.ContainerForumId -- forum1
+  ON Forum_hasMember_Person2.ForumId = Message1.ContainerForumId -- forum1
  AND Forum_hasMember_Person2.PersonId = Comment.CreatorPersonId -- person2
 -- (forum1)-[:Has_MEMBER]->(person3)
 JOIN Forum_hasMember_Person Forum_hasMember_Person3
-  ON Forum_hasMember_Person3.ForumId = Post1.ContainerForumId -- forum1
+  ON Forum_hasMember_Person3.ForumId = Message1.ContainerForumId -- forum1
  AND Forum_hasMember_Person3.PersonId = Message2.CreatorPersonId -- person3
-WHERE Tag.name = :tag
-  -- person2 != person3
-  AND Comment.CreatorPersonId != Message2.CreatorPersonId
+WHERE Message1.ContainerForumId <> Message2.ContainerForumId
   -- NOT (forum2)-[:HAS_MEMBER]->(person1)
   AND NOT EXISTS (SELECT 1
                   FROM Forum_hasMember_Person Forum_hasMember_Person1
-                  WHERE Forum_hasMember_Person1.ForumId = Post2.ContainerForumId -- forum2
+                  WHERE Forum_hasMember_Person1.ForumId = Message2.ContainerForumId -- forum2
                     AND Forum_hasMember_Person1.PersonId = Message1.CreatorPersonId -- person1
                  )
 GROUP BY Message1.CreatorPersonId
--- umbra/queries/bi-19.sql
+++ umbra/queries/bi-19.sql
@@ -8,72 +8,80 @@ qs(f, t) as (
     from Person p1, Person p2
     where p1.locationcityid = :city1Id and p2.locationcityid = :city2Id
 ),
-weights(src, dst, w) as (
+weights(src, dst, c) as (
     select least(m1.creatorpersonid, m2.creatorpersonid) as src,
            greatest(m1.creatorpersonid, m2.creatorpersonid) as dst,
-           1.0::double precision / count(*)
+           count(*) as c
     from Person_knows_person pp, Message m1, Message m2
     where pp.person1id = m1.creatorpersonid and pp.person2id = m2.creatorpersonid and m1.parentmessageid = m2.messageid and m1.creatorpersonid <> m2.creatorpersonid
     group by src, dst
 ),
 path(src, dst, w) as (
-    select src, dst, w from weights
+    select src, dst, 1.0::double precision / c from weights
     union all
-    select dst, src, w from weights
+    select dst, src, 1.0::double precision / c from weights
 ),
-shorts(gsrc, dst, w, dead, iter) as (
+shorts(dir, gsrc, dst, w, dead, iter) as (
     (
-        with srcs as (select distinct f from qs)
-        select f, f, 0, true, 0 from srcs
-        union all
-        select ss.f, p.dst, min(p.w), false, 0
-        from srcs ss, path p
-        where ss.f = p.src and p.dst not in (select ss2.f from srcs ss2)
-        group by ss.f, p.dst
+        with
+        srcs as (select distinct f from qs),
+        dsts as (select distinct t from qs)
+        (
+            select false, f, f, 0, false, 0 from srcs
+            union all
+            select true, t, t, 0, false, 0 from dsts
+        )
     )
     union all
     (
         with
         toExplore as (select * from shorts where dead = false order by w limit 1000),
+        -- assumes graph is undirected
         newPoints as (
-            select e.gsrc as gsrc, p.dst as dst, min(e.w + p.w) as w, false as dead, min(e.iter) + 1 as iter
+            select e.dir, e.gsrc as gsrc, p.dst as dst, min(e.w + p.w) as w, false as dead, min(e.iter) + 1 as iter
             from path p join toExplore e on forceorder(e.dst = p.src)
-            group by e.gsrc, p.dst
+            group by e.dir, e.gsrc, p.dst
         ),
         updated as (
-            select n.gsrc, n.dst, n.w, false as dead, iter
+            select n.dir, n.gsrc, n.dst, n.w, false as dead, iter
             from newPoints n
             where not exists (select * from shorts o where n.gsrc = o.gsrc and n.dst = o.dst and o.w <= n.w)
         ),
         found as (
-            select q.f, q.t, n.w
-            from qs q left join updated n on n.dst = q.t and n.gsrc = q.f
+            select min(l.w + r.w) as w
+            from shorts l, shorts r
+            where l.dir = false and r.dir = true and l.dst = r.dst
         ),
         ss2 as (
-            select o.gsrc, o.dst, o.w, o.dead or (exists (select * from toExplore e where e.gsrc = o.gsrc and e.dst = o.dst)) as dead, o.iter + 1 as iter
+            select o.dir, o.gsrc, o.dst, o.w, o.dead or (exists (select * from toExplore e where e.dir = o.dir and e.gsrc = o.gsrc and e.dst = o.dst)) as dead, o.iter + 1 as iter
             from shorts o
         ),
         fullTable as (
-            select coalesce(n.gsrc, o.gsrc) as gsrc,
+            select coalesce(n.dir, o.dir) as dir,
+                   coalesce(n.gsrc, o.gsrc) as gsrc,
                    coalesce(n.dst, o.dst) as dst,
                    coalesce(n.w, o.w) as w,
                    coalesce(n.dead, o.dead) as dead,
                    coalesce(n.iter, o.iter) as iter
-            from ss2 o full join updated n on o.gsrc = n.gsrc and o.dst = n.dst
+            from ss2 o full join updated n on o.dir = n.dir and o.gsrc = n.gsrc and o.dst = n.dst
         )
-        select gsrc,
+        select dir,
+               gsrc,
                dst,
                w,
-               dead or (t.w > coalesce((select min(w) from found f), t.w)),
+               dead or (coalesce(t.w > (select f.w/2 from found f), false)),
                iter
         from fullTable t
         where exists (select * from toExplore limit 1)
     )
 ),
-ss(gsrc, dst, w, iter) as (
-    select gsrc, dst, w, iter from shorts where iter = (select max(iter) from shorts)
+ss(dir, gsrc, dst, w, iter) as (
+    select dir, gsrc, dst, w, iter from shorts where iter = (select max(iter) from shorts)
 ),
 results(f, t, w) as (
-    select qs.f, qs.t , ss.w from qs left join ss on qs.f = ss.gsrc and qs.t = ss.dst
+    select l.gsrc, r.gsrc, min(l.w + r.w)
+    from ss l, ss r
+    where l.dir = false and r.dir = true and l.dst = r.dst
+    group by l.gsrc, r.gsrc
 )
 select * from results where w = (select min(w) from results) order by f, t;
