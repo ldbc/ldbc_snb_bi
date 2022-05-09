24cd55c0248b8662c2343dc566f6bcb90bfbcb29
Gabor Szarnyas <szarnyasg@gmail.com>
2022-05-09 17:53:43 +0200

    Tune paramgen

--- paramgen/paramgen-queries/pg-18.sql
+++ paramgen/paramgen-queries/pg-18.sql
@@ -4,7 +4,7 @@ FROM (
     SELECT
         tagName,
         frequency AS freq,
-        abs(frequency - (SELECT percentile_disc(0.75) WITHIN GROUP (ORDER BY frequency) FROM tagNumMessages)) AS diff
+        abs(frequency - (SELECT percentile_disc(0.97 ??) WITHIN GROUP (ORDER BY frequency) FROM tagNumMessages)) AS diff
     FROM tagNumMessages
     ORDER BY diff, tagName
     LIMIT 400
