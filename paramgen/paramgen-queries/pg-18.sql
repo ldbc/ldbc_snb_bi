bb31583e6235724176d61b96a81b3d0ad575a5c8
Gabor Szarnyas <szarnyasg@gmail.com>
2022-05-09 23:47:14 +0200

    Increase percentile

--- paramgen/paramgen-queries/pg-18.sql
+++ paramgen/paramgen-queries/pg-18.sql
@@ -4,7 +4,7 @@ FROM (
     SELECT
         tagName,
         frequency AS freq,
-        abs(frequency - (SELECT percentile_disc(0.96) WITHIN GROUP (ORDER BY frequency) FROM tagNumPersons)) AS diff
+        abs(frequency - (SELECT percentile_disc(0.99) WITHIN GROUP (ORDER BY frequency) FROM tagNumPersons)) AS diff
     FROM tagNumPersons
     ORDER BY diff, tagName
     LIMIT 400
