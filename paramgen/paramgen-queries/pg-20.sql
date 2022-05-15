868c3066eea4c97e349b26f994b95305057dff77
Gabor Szarnyas <szarnyasg@gmail.com>
2022-05-15 14:28:49 +0200

    Only allow 5 persons per company for BI Q20 parameters

--- paramgen/paramgen-queries/pg-20.sql
+++ paramgen/paramgen-queries/pg-20.sql
@@ -3,16 +3,24 @@ SELECT DISTINCT
     k2.person2Id AS 'person2Id:ID'
 FROM
     (SELECT
-        Person_workAt_Company_window.personId AS person1Id,
-        Person_workAt_Company_window.companyId AS companyId,
+        person1Id,
+        companyId,
         companyName,
-        frequency AS freq,
-        abs(frequency - (SELECT percentile_disc(0.47) WITHIN GROUP (ORDER BY frequency) FROM companyNumEmployees)) AS diff
-    FROM companyNumEmployees
-    JOIN Person_workAt_Company_window
-      ON Person_workAt_Company_window.companyId = companyNumEmployees.companyId
-    ORDER BY diff, md5(Person_workAt_Company_window.personId), md5(Person_workAt_Company_window.companyId)
-    LIMIT 2000
+      FROM (
+          SELECT
+              Person_workAt_Company_window.personId AS person1Id,
+              Person_workAt_Company_window.companyId AS companyId,
+              companyName,
+              frequency AS freq,
+              abs(frequency - (SELECT percentile_disc(0.47) WITHIN GROUP (ORDER BY frequency) FROM companyNumEmployees)) AS diff,
+              row_number() OVER (PARTITION BY Person_workAt_Company_window.companyId ORDER BY md5(Person_workAt_Company_window.personId)) AS rnum
+          FROM companyNumEmployees
+          JOIN Person_workAt_Company_window
+          ON Person_workAt_Company_window.companyId = companyNumEmployees.companyId
+          ORDER BY diff, md5(Person_workAt_Company_window.personId), md5(Person_workAt_Company_window.companyId)
+      )
+      WHERE rnum < 5
+      LIMIT 200
     ) comp
 -- ensure that there is a two-hop path
 -- hop 1
