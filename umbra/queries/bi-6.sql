a6944397796573c43d0fbc6769cefdc918dd769a
Altan Birler <altan.birler@tum.de>
2022-05-31 18:20:57 +0200

    Use precomputation in Umbra Q6

--- umbra/queries/bi-6.sql
+++ umbra/queries/bi-6.sql
@@ -14,20 +14,11 @@ WITH poster_w_liker AS (
                ON like2.MessageId = message1.MessageId
            -- we don't need the Person itself as its ID is in the like
          WHERE Tag.name = :tag
-    ),
-    popularity_score AS (
-        SELECT
-           message2.CreatorPersonId AS person2id,
-           count(*) AS popularityScore
-        FROM Message message2
-        JOIN Person_likes_Message like2
-          ON like2.MessageId = message2.MessageId
-        GROUP BY message2.CreatorPersonId
     )
 SELECT pl.person1id AS "person1.id",
        sum(coalesce(ps.popularityScore, 0)) AS authorityScore
 FROM poster_w_liker pl
-LEFT JOIN popularity_score ps
+LEFT JOIN PopularityScoreQ06 ps
          ON ps.person2id = pl.person2id
 GROUP BY pl.person1id
 ORDER BY authorityScore DESC, pl.person1id ASC
