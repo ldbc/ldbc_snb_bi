abfb94a44cc46a641c8dc606b0e27c2cf528cfd6
daniel <dljtw98@gmail.com>
2022-05-31 11:34:26 +0200

    Pushing experiments script file

--- duckdb/experiment_script.sh
+++ duckdb/experiment_script.sh
@@ -2,9 +2,9 @@
 
 source ../../CLionProjects/duckdb-pgq/.venv/bin/activate
 
-for _ in {1..5}
+for i in 10 3 1 0.3 0.1
 do
-  for i in 0.1 0.3 1 3 10
+  for j in {1..50}
   do
     echo $i $j
     python3 load.py -s $i -q 20 -l 0 -w bi
