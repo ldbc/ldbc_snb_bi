29da06a256a3027d04cb66c3d9971485a694eac8
daniel <dljtw98@gmail.com>
2022-06-15 16:48:38 +0200

    Writing to parquet

---
+++ duckdb/convert_to_parquet.py
@@ -0,0 +1,28 @@
+import logging
+import os
+
+from load import load_entities, run_script, process_arguments
+import duckdb
+import sys
+
+
+def main(argv):
+    sf, query, only_load, workload, lanes, threads, experimental_mode = process_arguments(argv)
+
+    for sf in [10]:
+        con = duckdb.connect("snb_benchmark.duckdb", read_only=False)
+        run_script(con, "ddl/drop-tables.sql")
+        run_script(con, "ddl/schema-composite-merged-fk.sql")
+        data_dir = f'../../ldbc_snb_datagen_spark/out-sf{sf}/graphs/csv/bi/composite-merged-fk'
+        load_entities(con, data_dir, query)
+        logging.debug("writing to parquet")
+        out_dir = f'../../ldbc_snb_datagen_spark/out-sf{sf}/parquet'
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        con.execute(f"EXPORT DATABASE '{out_dir}' (FORMAT PARQUET);")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s', level=logging.DEBUG)
+    main(sys.argv[1:])
