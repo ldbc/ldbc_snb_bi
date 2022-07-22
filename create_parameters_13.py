3e7368d4aebe4a0c61f5c105ef75aa8a69259a6f
dtenwolde <d.l.j.ten.wolde@student.vu.nl>
2022-07-22 16:34:18 +0200

    Modified parameters 13 flipped

--- create_parameters_13.py
+++ create_parameters_13.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-sf = '10000'
 
-df = pd.read_csv(f'/export/scratch2/dljtw/ldbc_snb_bi/parameters/parameters-sf{sf}/bi-15a.csv', sep="|")
 
-df = df.drop(columns=['startDate:DATE', 'endDate:DATE'])
+sfs = ['1', '3', '10', '30', '100', '300', '1000']
 
-df.to_csv(f'/export/scratch2/dljtw/ldbc_snb_bi/parameters/parameters-sf{sf}/interactive-13.csv', index=False, sep="|")
+for sf in sfs: 
+
+    df = pd.read_csv(f'/export/scratch2/dljtw/ldbc_snb_bi/parameters/parameters-sf{sf}/bi-15a.csv', sep="|")
+
+    df = df.drop(columns=['startDate:DATE', 'endDate:DATE'])
+    df.rename(columns={'person1Id:ID': 'person2Id:ID', 'person2Id:ID': 'person1Id:ID'}, inplace=True)
+    # df.rename(columns={'person2id:ID_': 'person2id:ID'}, inplace=True)
+    df = df[df.columns[::-1]]
+    df.to_csv(f'/export/scratch2/dljtw/ldbc_snb_bi/parameters/parameters-sf{sf}/interactive-13.csv', index=False, sep="|")
