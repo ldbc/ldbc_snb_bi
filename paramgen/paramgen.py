3f99df03d48ea6fed7e600728c998b4b1fbeb97f
Gabor Szarnyas <szarnyasg@gmail.com>
2022-08-21 15:44:52 +0200

    Paramgen: Add Q20a and Q20b variants. Fixes #77

--- paramgen/paramgen.py
+++ paramgen/paramgen.py
@@ -12,7 +12,7 @@ with open(f"paramgen-queries/ddl/schema.sql", "r") as schema_file:
 
 print()
 print("============ Loading the factor tables ============")
-for entity in ["cityNumPersons", "cityPairsNumFriends", "companyNumEmployees", "countryNumMessages", "countryNumPersons", "countryPairsNumFriends", "creationDayAndLengthCategoryNumMessages", "creationDayAndTagNumMessages", "creationDayAndTagClassNumMessages", "creationDayNumMessages", "languageNumPosts", "lengthNumMessages", "people2Hops", "people4Hops", "personDisjointEmployerPairs", "personNumFriends", "tagClassNumMessages", "tagClassNumTags", "tagNumMessages", "tagNumPersons"]:
+for entity in ["cityNumPersons", "cityPairsNumFriends", "companyNumEmployees", "countryNumMessages", "countryNumPersons", "countryPairsNumFriends", "creationDayAndLengthCategoryNumMessages", "creationDayAndTagNumMessages", "creationDayAndTagClassNumMessages", "creationDayNumMessages", "languageNumPosts", "lengthNumMessages", "people2Hops", "people4Hops", "personDisjointEmployerPairs", "personNumFriends", "tagClassNumMessages", "tagClassNumTags", "tagNumMessages", "tagNumPersons", "sameUniversityConnected"]:
     print(f"{entity}")
     parquet_files = [f for f in os.listdir(f"{factor_parquet_path}{entity}/") if f.endswith(".parquet")]
     if not parquet_files:
@@ -96,8 +96,8 @@ for query_variant in ["2d", "2m", "8d", "8m", "20d", "20m"]:
 
 print()
 print("============ Generating parameters ============")
-for query_variant in ["1", "2a", "2b", "3", "4", "5", "6", "7", "8a", "8b", "9", "10a", "10b", "11", "12", "13", "14a", "14b", "15a", "15b", "16a", "16b", "17", "18", "19a", "19b", "20"]:
+for query_variant in ["1", "2a", "2b", "3", "4", "5", "6", "7", "8a", "8b", "9", "10a", "10b", "11", "12", "13", "14a", "14b", "15a", "15b", "16a", "16b", "17", "18", "19a", "19b", "20a", "20b"]:
     print(f"- Q{query_variant}")
     with open(f"paramgen-queries/pg-{query_variant}.sql", "r") as parameter_query_file:
         parameter_query = parameter_query_file.read()
-        con.execute(f"COPY ( {parameter_query} ) TO '../parameters/bi-{query_variant}.csv' WITH (HEADER, DELIMITER '|');")
+        con.execute(f"COPY ({parameter_query}) TO '../parameters/bi-{query_variant}.csv' WITH (HEADER, DELIMITER '|');")
