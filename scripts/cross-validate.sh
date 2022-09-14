8d2e9e384f26cc0d68bfb19bc3979a7fec149477
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-14 14:31:19 +0200

    Add usage to cross-validate.sh script

--- scripts/cross-validate.sh
+++ scripts/cross-validate.sh
@@ -6,6 +6,11 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
+if [ "${#}" -ne 2 ]; then
+    echo "Usage: cross-validate.sh <system1> <system2>"
+    exit 1
+fi
+
 numdiff \
     --separators='\|\n;,<>' \
     --absolute-tolerance 0.001 \
