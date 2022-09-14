4d52ef4df7e3c659da6826ef78efccf0caad1ae1
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-14 16:46:19 +0200

    Cypher: Add script to use parallel runtime where possible

---
+++ cypher/scripts/use-parallel-runtime.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Use the Neo4j parallel runtime, available in the Enterprise Edition, where possible.
+# Note that unlike the Community Edition, the Enterprise Edition is not free. Please ensure you have a license before using it.
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. scripts/vars.sh
+
+sed -i '1 i\CYPHER runtime=parallel' queries/bi-{1,3,4,5,6,7,8,9,11,12,13,14,18}.cypher
+sed -i '1 i\CYPHER runtime=pipelined' queries/bi-{2,10,15,16,17,19,20}.cypher
