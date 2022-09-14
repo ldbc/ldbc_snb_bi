910287b50eb79fefa8e7601d66babe05e113282b
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-14 16:03:39 +0200

    Umbra: Add script to decompress .csv.gz files

---
+++ umbra/scripts/decompress-data-set.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+cd ${UMBRA_CSV_DIR}
+
+# decompress the CSVs but keep the original .csv.gz files
+find . -name *.csv.gz | xargs -I {} gunzip --keep {}
--- umbra/scripts/get-sample-data-set.sh
+++ umbra/scripts/get-sample-data-set.sh
@@ -6,7 +6,13 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-rm -f social-network-sf0.003-bi-composite-merged-fk.zip
-wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-composite-merged-fk.zip
-rm -rf social-network-sf0.003-bi-composite-merged-fk/
-unzip -q social-network-sf0.003-bi-composite-merged-fk.zip
+export SAMPLE_DATA_SET_NAME=social-network-sf0.003-bi-composite-merged-fk-postgres-compressed
+rm -f ${SAMPLE_DATA_SET_NAME}.zip
+wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/${SAMPLE_DATA_SET_NAME}.zip
+rm -rf ${SAMPLE_DATA_SET_NAME}/
+unzip -q ${SAMPLE_DATA_SET_NAME}.zip
+
+export SF=0.003
+. scripts/use-sample-data-set.sh
+
+scripts/decompress-data-set.sh
