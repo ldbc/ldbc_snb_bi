7455525faa6865f3ae3ba8e158869ca0893306c2
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-20 21:36:14 +0200

    Cypher: Export SF in use-sample-data-set.sh script

--- cypher/scripts/use-sample-data-set.sh
+++ cypher/scripts/use-sample-data-set.sh
@@ -1,1 +1,2 @@
-export NEO4J_CSV_DIR=`pwd`/social-network-sf0.003-bi-composite-projected-fk-neo4j-compressed/graphs/csv/bi/composite-projected-fk/
+export SF=0.003
+export NEO4J_CSV_DIR=`pwd`/social-network-sf${SF}-bi-composite-projected-fk-neo4j-compressed/graphs/csv/bi/composite-projected-fk/
