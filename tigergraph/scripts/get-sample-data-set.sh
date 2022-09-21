50b0aa8f302f621a57d4c75b2e4477c5c86d50b2
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-21 20:35:28 +0200

    TigerGraph: Cleanup in get-sample-data-set.sh script

--- tigergraph/scripts/get-sample-data-set.sh
+++ tigergraph/scripts/get-sample-data-set.sh
@@ -5,8 +5,9 @@ pushd . > /dev/null
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-
+rm -f social-network-sf0.003-bi-composite-projected-fk-compressed.zip
 wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-composite-projected-fk-compressed.zip
+rm -rf social-network-sf0.003-bi-composite-projected-fk-compressed/
 unzip -q social-network-sf0.003-bi-composite-projected-fk-compressed.zip
 
 popd > /dev/null
