8e78c38cde07f92e74f882a3324aa1b6e890cc1b
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-27 00:35:04 +0200

    Paramgen: Delete zip file before downloading it again

--- paramgen/scripts/get-sample-factors.sh
+++ paramgen/scripts/get-sample-factors.sh
@@ -6,7 +6,11 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
+export FACTOR_DIRNAME=social-network-sf0.003-bi-factors
+
 rm -rf factors/*
-wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/social-network-sf0.003-bi-factors.zip
-unzip -q social-network-sf0.003-bi-factors.zip
-cp -r social-network-sf0.003-bi-factors/factors/parquet/raw/composite-merged-fk/* factors/
+rm -f ${FACTOR_DIRNAME}.zip
+wget -q https://ldbcouncil.org/ldbc_snb_datagen_spark/${FACTOR_DIRNAME}.zip
+rm -rf ${FACTOR_DIRNAME}
+unzip -q ${FACTOR_DIRNAME}.zip
+cp -r ${FACTOR_DIRNAME}/factors/parquet/raw/composite-merged-fk/* factors/
