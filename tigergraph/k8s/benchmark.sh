f0f5c0e5dcda70a1b0a1951a3b673c24472565aa
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2022-10-06 04:28:17 +0000

    fix delete Person problem;

--- tigergraph/k8s/benchmark.sh
+++ tigergraph/k8s/benchmark.sh
@@ -5,4 +5,4 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . vars.sh
 cd ..
-python3 -u benchmark.py ${TG_DATA_DIR} --cluster --para ${TG_PARAMETER} --scale_factor ${SF} $@
+python3 -u benchmark.py --cluster --scale_factor ${SF} --data_dir ${TG_DATA_DIR} --para ${TG_PARAMETER} --endpoint ${TG_ENDPOINT} $@
--- tigergraph/k8s/vars.sh
+++ tigergraph/k8s/vars.sh
@@ -3,6 +3,9 @@ pushd . > /dev/null
 cd "$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
+export TG_REST_PORT=9000
+export TG_ENDPOINT=http://127.0.0.1:${TG_REST_PORT}
+
 # SF-100 benchmark
 export NUM_NODES=2 # number of pods or nodes
 export SF=100 # data source 100, 1000, 3000, 10000 ...
