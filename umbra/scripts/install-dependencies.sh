42884b14592c6bff971eaab7f493f5320adbd67d
Gabor Szarnyas <szarnyasg@gmail.com>
2022-10-08 17:43:26 +0200

    Umbra: Add GNU Parallel to dependencies

--- umbra/scripts/install-dependencies.sh
+++ umbra/scripts/install-dependencies.sh
@@ -6,10 +6,10 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [[ ! -z $(which yum) ]]; then
-    sudo yum install -y python3-pip postgresql-devel postgresql sysstat
+    sudo yum install -y python3-pip postgresql-devel postgresql sysstat parallel
 elif [[ ! -z $(which apt-get) ]]; then
     sudo apt-get update
-    sudo apt-get install -y python3-pip libpq-dev postgresql-client sysstat
+    sudo apt-get install -y python3-pip libpq-dev postgresql-client sysstat parallel
 else
     echo "Operating system not supported, please install the dependencies manually"
 fi
