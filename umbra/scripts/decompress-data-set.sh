a710e9f02e64d53521ba34d81c3cb136c4ca98d1
Gabor Szarnyas <szarnyasg@gmail.com>
2022-10-08 17:47:33 +0200

    Umbra: Parallelize decompression of .csv.gz files

--- umbra/scripts/decompress-data-set.sh
+++ umbra/scripts/decompress-data-set.sh
@@ -6,7 +6,4 @@ set -o pipefail
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
-cd ${UMBRA_CSV_DIR}
-
-# decompress the CSVs but keep the original .csv.gz files
-find . -name *.csv.gz | xargs -I {} gunzip {}
+find ${UMBRA_CSV_DIR} -name "*.csv.gz"  -print0 | parallel -q0 gunzip
