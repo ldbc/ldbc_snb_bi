45f9c80dfa145ee74e6e45c2190ce0a2194eb8b0
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2022-10-26 18:36:08 +0000

    non-docker setup output load.csv

--- tigergraph/k8s/setup.sh
+++ tigergraph/k8s/setup.sh
@@ -6,4 +6,18 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
 . k8s/vars.sh
+
+if [ "$(uname)" == "Darwin" ]; then
+    DATE_COMMAND=gdate
+else
+    DATE_COMMAND=date
+fi
+
+start_time=$(${DATE_COMMAND} +%s.%3N)
+
+
 . $DDL_PATH/setup.sh $TG_DATA_DIR $QUERY_PATH $DML_PATH
+
+
+end_time=$(${DATE_COMMAND} +%s.%3N)
+echo -e "time\n${elapsed}" > output/output-sf${SF}/load.csv
