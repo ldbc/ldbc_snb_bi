1d7fed85d54ad9b3d3da4bcc59e0ec394437c786
truman-wang <truman.wang@tigergraph.com>
2022-11-12 05:01:30 +0000

    add data downloading and decompression time

--- tigergraph/k8s/download_decompress.sh
+++ tigergraph/k8s/download_decompress.sh
@@ -17,8 +17,13 @@ sudo $installer install -y python3-pip parallel gzip
 sudo pip3 install google-cloud-storage
 
 echo "download SF$SF($i/$NUM_NODES) using $DOWNLOAD_THREAD threads"
+time1=$SECONDS
 python3 -u ${mydir}/download_one_partition.py $SF $i $NUM_NODES --target $target -t $DOWNLOAD_THREAD $SERVICE_KEY && \
 echo 'done download' && \
+time2=$SECONDS
 echo "decompose files in $target/sf$SF" && \
 find $target/sf$SF -name *.csv.gz  -print0 | parallel -q0 gunzip && \
 echo 'download and decompress finished'
+time3=$SECONDS
+echo "Data Downloading:        $((time2-time1)) s"
+echo "Data Decompression:           $((time3-time2)) s"
