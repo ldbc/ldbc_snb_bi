1c9356fd3f798b11bd5e70247faa56d99661b6d7
Yuchen Zhang <yuchen.zhang@tigergraph.com>
2022-11-23 19:48:34 +0000

    fix AWS setup

--- tigergraph/benchmark_on_cluster/setup_AWS.sh
+++ tigergraph/benchmark_on_cluster/setup_AWS.sh
@@ -2,7 +2,7 @@
 # !/usr/bin/sh
 key=$1
 ip_list_file=$2
-for ip in $ip_list_file
+for ip in $(cat $ip_list_file)
 do 
   scp -i $key setup.sh centos@$ip:~
   ssh -i $key centos@$ip "nohup sh setup.sh > foo.out 2>&1 < /dev/null & "
