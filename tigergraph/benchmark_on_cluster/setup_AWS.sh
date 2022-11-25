32241ffac5b57d68d2f9f3b6cdd8bbced3c819b4
truman-wang <truman.wang@tigergraph.com>
2022-11-25 09:50:56 +0000

    optimize ssh connection

--- tigergraph/benchmark_on_cluster/setup_AWS.sh
+++ tigergraph/benchmark_on_cluster/setup_AWS.sh
@@ -4,6 +4,6 @@ key=$1
 ip_list_file=$2
 for ip in $(cat $ip_list_file)
 do 
-  scp -i $key setup.sh ec2-user@$ip:~
+  scp -i $key -o StrictHostKeyChecking=accept-new setup.sh ec2-user@$ip:~
   ssh -i $key ec2-user@$ip "nohup sh setup.sh > foo.out 2>&1 < /dev/null & "
 done
