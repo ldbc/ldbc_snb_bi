2c97d7202fc3f8f85ae68b1ca17b2dc64b47cbcd
Altan Birler <altan.birler@tum.de>
2022-12-14 16:29:03 +0000

    update umbra version

--- umbra/scripts/vars.sh
+++ umbra/scripts/vars.sh
@@ -8,7 +8,7 @@ export UMBRA_DATABASE_DIR=`pwd`/scratch/db/
 export UMBRA_LOG_DIR=`pwd`/scratch/log/
 export UMBRA_DDL_DIR=`pwd`/ddl/
 export UMBRA_CONTAINER_NAME=snb-bi-umbra
-export UMBRA_VERSION=f87f59ea2
+export UMBRA_VERSION=7632149e3
 export UMBRA_DOCKER_IMAGE=umbra-release:${UMBRA_VERSION}
 
 cd scripts
