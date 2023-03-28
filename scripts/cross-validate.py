f7c1c413ded062b2861ffdac07c2c0eaf7ddc976
Gabor Szarnyas <szarnyasg@gmail.com>
2023-03-28 19:29:40 +0200

    Improve validation framework by detecting/counting skipped lines and misaligned parameters

--- scripts/cross-validate.py
+++ scripts/cross-validate.py
@@ -8,59 +8,117 @@ from collections import Counter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--scale-factor', type=str, help='Scale Factor', required=True)
-parser.add_argument('--tool1', type=str, help='The name of tool 1', required=True)
-parser.add_argument('--tool2', type=str, help='The name of tool 2', required=True)
-parser.add_argument('--output1', type=Path, help='The output path of tool 1', required=True)
-parser.add_argument('--output2', type=Path, help='The output path of tool 2', required=True)
+parser.add_argument('--tool-expected', type=str, help='The name of the tool producting the expected output', required=True)
+parser.add_argument('--tool-actual', type=str, help='The name of tool producing the actual output', required=True)
+parser.add_argument('--output-expected', type=Path, help='The output path of the expected output', required=True)
+parser.add_argument('--output-actual', type=Path, help='The output path of the actual output', required=True)
 parser.add_argument('--verbose', action='store_true', help='Verbose mode: print outputs of both tools if results are different')
 args = parser.parse_args()
 
 number_of_query_instances = Counter()
 validation_errors = Counter()
+skipped_lines = Counter()
+misaligned_parameters = Counter()
 
-with open(args.output1) as outputfile1, open(args.output2) as outputfile2: 
-    for line1, line2 in zip(outputfile1, outputfile2):
-        line1split = line1.strip().split("|")
-        line2split = line2.strip().split("|")
+print("========== Validation errors ===========")
+
+with open(args.output_expected) as outputfile_expected, open(args.output_actual) as outputfile_actual:
+    lines_expected = outputfile_expected.readlines()
+    lines_actual = outputfile_actual.readlines()
+    
+    i = 0
+    j = 0
+    num_skipped = 0
+    query_variant = 0
+    num_misaligned_parameters = 0
+    while i < len(lines_expected) and j < len(lines_actual):
+        line_expectedsplit = lines_expected[i].strip().split("|")
+        line_actualsplit = lines_actual[j].strip().split("|")
+
+        (query_expected, query_variant_expected, parameters_expected) = tuple(line_expectedsplit[0:3])
+        result_expected = json.loads(line_expectedsplit[3])
         
-        query = line1split[0]
-        query_variant = line1split[1]
-        parameters = line1split[2]
-        result1 = json.loads(line1split[3])
-        result2 = json.loads(line2split[3])
+        (query_actual, query_variant_actual, parameters_actual) = tuple(line_actualsplit[0:3])
+        result_actual = json.loads(line_actualsplit[3])
 
-        diff = recursive_diff(result1, result2, abs_tol=0.00001)
+        # checking whether the query variants align in the two inputs
+        if query_variant_expected > query_variant_actual:
+            j = j + 1
+            num_skipped = num_skipped + 1
+            continue
+
+        # we can save the query variant, the number of num_skipped lines, and advance to the next line
+        query_variant = query_variant_expected
+
+        if num_skipped > 0:
+            skipped_lines[query_variant] = num_skipped
+            print(f"* Q{query_variant_actual}: The output of tool '{args.tool_expected}' has {num_skipped} fewer entry/entries than tool '{args.tool_actual}'.")
+            num_skipped = 0
+
+        i = i + 1
+        j = j + 1
+
+        # checking whether the parameters align for the same query variant in the current row
+        if parameters_expected != parameters_actual:
+            num_misaligned_parameters = num_misaligned_parameters + 1
+            continue
+
+        if num_misaligned_parameters > 0:
+            misaligned_parameters[query_variant] = num_misaligned_parameters
+            num_misaligned_parameters = 0
+            print(f"* Q{query_variant}: {misaligned_parameters[query_variant]} parameters were different, these were skipped.")
+
+        parameters = parameters_expected
+
+        diff = recursive_diff(result_expected, result_actual, abs_tol=0.00001)
         diff = peekable(diff)
-        
+
         number_of_query_instances[query_variant] += 1
         if diff:
             print(textwrap.dedent(f"""
-                Outputs differ for:
-                - Query variant: Q{query_variant}
-                - Input parameters: {parameters}
-                Difference(s):
+                * Q{query_variant}: Outputs differ for inputs {parameters}:
                 """).strip())
 
             for d in diff:
-                print(f"- {d}")
+                print(f"  - {d}")
 
             if args.verbose:
                 print(textwrap.dedent(f"""
-                Output of tool '{args.tool1}':
-                {result1}
+                Output of tool '{args.tool_expected}':
+                {result_expected}
 
-                Output of tool '{args.tool2}':
-                {result2}
+                Output of tool '{args.tool_actual}':
+                {result_actual}
                 """).strip())
-            print()
+                print()
 
             validation_errors[query_variant] += 1
 
+print()
+print("========== Validation summary ==========")
+
+if skipped_lines:
+    print(f"Skipped a total of {sum(skipped_lines.values())} line(s):")
+    for q, num in skipped_lines.items():
+        print(f"- Q{q}: {num} lines skipped")
+    print()
+
+if misaligned_parameters:
+    print(f"Found {sum(misaligned_parameters.values())} misaligned input parameters:")
+    for q, num in misaligned_parameters.items():
+        total = number_of_query_instances[q]
+        print(f"- Q{q}: {num} parameters misaligned out of {total} query instances")
+    print()
+
 if validation_errors:
-    print(f"Validation failed with validation error(s)")
+    print(f"Found {sum(validation_errors.values())} validation error(s):")
     for q, num in validation_errors.items():
-        total = number_of_query_instances[query_variant]
-        print(f"- Q{query_variant}: {num} failed out of {total} query instances")
+        total = number_of_query_instances[q]
+        print(f"- Q{q}: {num} failed out of {total} query instances")
+    print()
+
+if skipped_lines or validation_errors:
+    print("Validation failed.")
     exit(-1)
-else:
-    print("Validation passed")
+
+print("Validation passed.")
--- scripts/cross-validate.sh
+++ scripts/cross-validate.sh
@@ -7,14 +7,14 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
 if [ "${#}" -lt 2 ]; then
-    echo "Usage: cross-validate.sh <tool1> <tool2> [--verbose]"
+    echo "Usage: cross-validate.sh <tool_expected> <tool_actual> [--verbose]"
     exit 1
 fi
 
 python3 scripts/cross-validate.py \
     --scale-factor ${SF} \
-    --tool1 ${1} \
-    --tool2 ${2} \
-    --output1 ${1}/output/output-sf${SF}/results.csv \
-    --output2 ${2}/output/output-sf${SF}/results.csv \
+    --tool-expected ${1} \
+    --tool-actual ${2} \
+    --output-expected ${1}/output/output-sf${SF}/results.csv \
+    --output-actual ${2}/output/output-sf${SF}/results.csv \
     ${@:3}
