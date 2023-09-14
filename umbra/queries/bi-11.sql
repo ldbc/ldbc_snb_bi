ca486cbe7d2552608b746e11e6377ce3bf48a81c
Gabor Szarnyas <gabor@duckdblabs.com>
2023-09-14 11:27:29 +0200

    Umbra Q11: Fix comment

--- umbra/queries/bi-11.sql
+++ umbra/queries/bi-11.sql
@@ -27,7 +27,7 @@ SELECT count(*)
     -- filter: unique triangles only
    AND p1.PersonId < p2.PersonId
    AND p2.PersonId < p3.PersonId
-    -- filter: only edges created after :startDate
+    -- filter: only edges created after :startDate and before :endDate
    AND :startDate <= p1.creationDate AND p1.creationDate <= :endDate
    AND :startDate <= p2.creationDate AND p2.creationDate <= :endDate
    AND :startDate <= p3.creationDate AND p3.creationDate <= :endDate
