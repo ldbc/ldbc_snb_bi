e998e57c873ea8e5c106c32d6ae66814402aa591
Gabor Szarnyas <szarnyasg@gmail.com>
2024-06-13 18:26:36 +0200

    Add ::VARCHAR casts to naive paramgen queries

--- naive-paramgen/paramgen-queries/pg-20b.sql
+++ naive-paramgen/paramgen-queries/pg-20b.sql
@@ -3,6 +3,6 @@ SELECT
     personNumFriends_sample.id AS 'person2Id:ID'
 FROM
     companyNumEmployees,
-    (SELECT id FROM personNumFriends ORDER BY md5(id) LIMIT 100) personNumFriends_sample
+    (SELECT id FROM personNumFriends ORDER BY md5(id::VARCHAR) LIMIT 100) personNumFriends_sample
 ORDER BY md5((3532569367::bigint*companyNumEmployees.id + 211::bigint*personNumFriends_sample.id)::VARCHAR)
 LIMIT 400
