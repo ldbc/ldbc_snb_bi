dd140c921d5d23d9a9ebb098cbd92ade11e63f7f
Gabor Szarnyas <szarnyasg@gmail.com>
2024-06-13 18:15:37 +0200

    Add ::VARCHAR casts to naive paramgen queries

--- naive-paramgen/paramgen-queries/pg-10b.sql
+++ naive-paramgen/paramgen-queries/pg-10b.sql
@@ -6,7 +6,7 @@ SELECT
     4 AS 'maxPathDistance:INT'
 FROM
     countryNumPersons,
-    (SELECT * FROM personNumFriends ORDER BY md5(id) LIMIT 100) personNumFriends_sample,
+    (SELECT * FROM personNumFriends ORDER BY md5(id::VARCHAR) LIMIT 100) personNumFriends_sample,
     tagClassNumTags
 ORDER BY md5(concat(personNumFriends_sample.id, countryNumPersons.name, tagClassNumTags.name))
 LIMIT 400
